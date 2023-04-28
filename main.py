d9a73e4cf8811b765d5460e8e8135e2712e2c1cd
daithileonard <DAITHILEONARD@GMAIL.COM>
2023-04-28 14:52:53 +0100

    Added textfield, slider and dropdown

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import streamlit as st
+import pandas as pt
+
+st.set_page_config(layout="wide")
+st.title("Weather Forecast for the Next Week")
+
+city = st.text_input("Place:")
+days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days")
+option = st.selectbox("Select data to view", ("Temperature", "Sky"))
+
+st.subheader(f"{option} for the next {days} days in {city}")
